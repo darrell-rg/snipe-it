27e6b521c42440c49b2dafadab1d68ba98375553
Darrell Taylor <darrell@sawpit.app>
2023-08-21 15:00:01 -0600

    rename file

--- rmjImport/restoreIntitalState.sh
+++ rmjImport/restoreIntitalState.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -x #echo on
 
-#make sure you run this with source: or the paths in the zip file will be messed up
+echo make sure you run this with: sudo source restoreIntitalState.sh
 
 cd /var/www/html/snipeit/rmjImport
 
 zip -u snipe-it-dev2-backup-23-08-21-12-16-19.zip /var/www/html/snipeit/.env
 zip -u snipe-it-dev2-backup-23-08-21-12-16-19.zip db-dumps/mysql-snipeit.sql
 zip -d snipe-it-dev2-backup-23-08-21-12-16-19.zip /var/www/html/snipeit/storage/oauth-public.key
+zip -d snipe-it-dev2-backup-23-08-21-12-16-19.zip /var/www/html/snipeit/storage/oauth-private.key
 zipinfo snipe-it-dev2-backup-23-08-21-12-16-19.zip
-
-
 
 
 php ../artisan snipeit:restore /var/www/html/snipeit/rmjImport/snipe-it-dev2-backup-23-08-21-12-16-19.zip
